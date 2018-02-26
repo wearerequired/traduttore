f109e90f56349d8abfe91cca158b8f6bc8ebd02b
Pascal Birchler <hello@pascalbirchler.ch>
2018-02-26 23:13:39 +0100

    Add lock mechanism

--- inc/GitHubUpdater.php
+++ inc/GitHubUpdater.php
@@ -20,6 +20,13 @@ class GitHubUpdater {
 	/**
 	 * @since 2.0.0
 	 *
+	 * @var string Lock meta key.
+	 */
+	const LOCK_KEY = '_traduttore_update_lock';
+
+	/**
+	 * @since 2.0.0
+	 *
 	 * @var string Repository SSH URL.
 	 */
 	protected $ssh_url;
@@ -88,15 +95,25 @@ class GitHubUpdater {
 		$git_target = get_temp_dir() . 'traduttore-github-' . $slug;
 		$pot_target = wp_tempnam( 'traduttore-' . $slug . '.pot' );
 
+		if ( $this->has_lock() ) {
+			return false;
+		}
+
+		$this->add_lock();
+
 		$result = $this->fetch_github_repository( $this->ssh_url, $git_target );
 
 		if ( ! $result ) {
+			$this->remove_lock();
+
 			return false;
 		}
 
 		$result = $this->create_pot_file( $git_target, $pot_target, $slug );
 
 		if ( ! $result ) {
+			$this->remove_lock();
+
 			return false;
 		}
 
@@ -106,10 +123,14 @@ class GitHubUpdater {
 		unlink( $pot_target );
 
 		if ( ! $result ) {
+			$this->remove_lock();
+
 			return false;
 		}
 
 		$stats = GP::$original->import_for_project( $this->project, $translations );
+
+		$this->remove_lock();
 
 		/**
 		 * Fires after translations have been updated from GitHub.
@@ -120,7 +141,7 @@ class GitHubUpdater {
 		 * @param array      $stats        Stats about the number of imported translations.
 		 * @param PO         $translations PO object containing all the translations from the POT file.
 		 */
-		do_action( 'traduttore_updated_from_github', $this->project, $stats, $translations );
+		//do_action( 'traduttore_updated_from_github', $this->project, $stats, $translations );
 
 		return true;
 	}
@@ -164,5 +185,35 @@ class GitHubUpdater {
 		exec( escapeshellcmd( sprintf( 'wp i18n make-pot %1$s %2$s --slug=%3$s --domain=%3$s', $source, $target, $slug ) ), $output, $status );
 
 		return 0 === $status;
+	}
+
+	/**
+	 * Adds a lock to the current project to prevent two simultaneous imports.
+	 *
+	 * @since 2.0.0
+	 */
+	protected function add_lock() {
+		gp_update_meta( $this->project->id, static::LOCK_KEY, 1, 'project' );
+	}
+
+	/**
+	 * Checks whether an import is currently in progress or not.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @return bool Whether the project is locked.
+	 */
+	protected function has_lock(  ) {
+		return (bool) gp_get_meta( 'project', $this->project->id, static::LOCK_KEY );
+	}
+
+	/**
+	 * Removes the lock for the current project.
+	 *
+	 * @since 2.0.0
+	 */
+	protected function remove_lock() {
+		// gp_delete_meta() is internal.
+		gp_update_meta( $this->project->id, static::LOCK_KEY, 0, 'project' );
 	}
 }
