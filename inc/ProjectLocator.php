wearerequired/traduttore

commit 17f3ba46255cecb1109338b15f406f129d55faca

Add ProjectLocator helper class to make things DRY

inc/ProjectLocator.php
@@ -1,0 +1,68 @@
+<?php
+/**
+ * ProjectLocator class.
+ *
+ * @since 2.0.0
+ */
+
+namespace Required\Traduttore;
+
+use GP;
+use GP_Project;
+use PO;
+
+/**
+ * Helper class to find a GlotPress project based on path, ID, or GitHub repository URL.
+ *
+ * @since 2.0.0
+ */
+class ProjectLocator {
+	/**
+	 * @var string|int Project information.
+	 */
+	protected $project;
+
+	/**
+	 * ProjectLocator constructor.
+	 *
+	 * @param string|int $project Project information.
+	 */
+	public function __construct( $project ) {
+		$this->project = $project;
+	}
+
+	/**
+	 * Returns the found project.
+	 *
+	 * @return GP_Project|false GlotPress project on success, false otherwise.
+	 */
+	public function get_project() {
+		$project = GP::$project->by_path( $this->project );
+
+		if ( is_numeric( $this->project ) ) {
+			$project = GP::$project->get( (int) $this->project );
+		}
+
+		if ( ! $project ) {
+			$project = GitHubUpdater::find_project( $this->project );
+		}
+
+		return $project;
+	}
+
+	/**
+	 * Finds a GlotPress project by a GitHub repository URL, e.g. https://github.com/wearerequired/required-valencia.
+	 *
+	 * @since 2.0.0
+	 * @return false|GP_Project Project on success, false otherwise.
+	 */
+	protected function find_project() {
+		global $wpdb;
+
+		$table = GP::$project->table;
+
+		$query = $wpdb->prepare( "SELECT * FROM $table WHERE source_url_template LIKE %s LIMIT 1", '%' . $wpdb->esc_like( $this->project ) . '%' );
+
+		return GP::$project->coerce( $wpdb->get_row( $query ) );
+	}
+}

inc/CLI_Command.php
@@ -81,15 +81,8 @@ class CLI_Command extends WP_CLI_Command {
 	 *     Success: Updated translations for project (ID: 123)!
 	 */
 	public function update( $args, $assoc_args ) {
-		if ( is_numeric( $args[0] ) ) {
-			$project = GP::$project->get( (int) $args[0] );
-		} else {
-			$project = GP::$project->by_path( $args[0] );
-
-			if ( ! $project ) {
-				$project = GitHubUpdater::find_project( $args[0] );
-			}
-		}
+		$locator = new ProjectLocator( $args[0] );
+		$project = $locator->get_project();
 
 		if ( ! $project ) {
 			WP_CLI::error( 'Project not found' );
@@ -132,15 +125,8 @@ class CLI_Command extends WP_CLI_Command {
 	 *     Success: Removed cached Git repository for project (ID: 123)!
 	 */
 	public function clear_cache( $args, $assoc_args ) {
-		if ( is_numeric( $args[0] ) ) {
-			$project = GP::$project->get( (int) $args[0] );
-		} else {
-			$project = GP::$project->by_path( $args[0] );
-
-			if ( ! $project ) {
-				$project = GitHubUpdater::find_project( $args[0] );
-			}
-		}
+		$locator = new ProjectLocator( $args[0] );
+		$project = $locator->get_project();
 
 		if ( ! $project ) {
 			WP_CLI::error( 'Project not found' );

inc/GitHubUpdater.php
@@ -43,24 +43,6 @@ class GitHubUpdater {
 	}
 
 	/**
-	 * Find a GlotPress project by a GitHub repository URL.
-	 *
-	 * @since 2.0.0
-	 *
-	 * @param string $repository GitHub repository URL, e.g. https://github.com/wearerequired/required-valencia.
-	 * @return false|GP_Project Project on success, false otherwise.
-	 */
-	public static function find_project( $repository ) {
-		global $wpdb;
-
-		$table = GP::$project->table;
-
-		$query = $wpdb->prepare( "SELECT * FROM $table WHERE source_url_template LIKE %s LIMIT 1", '%' . $wpdb->esc_like( $repository ) . '%' );
-
-		return GP::$project->coerce( $wpdb->get_row( $query ) );
-	}
-
-	/**
 	 * Returns the repository URL based on the project's source URL template.
 	 *
 	 * @since 2.0.0

inc/Plugin.php
@@ -202,7 +202,12 @@ class Plugin {
 			return new WP_REST_Response( [ 'result' => 'Not the default branch' ] );
 		}
 
-		$project = GitHubUpdater::find_project( $params['repository']['html_url'] );
+		$locator = new ProjectLocator( $params['repository']['html_url'] );
+		$project = $locator->get_project();
+
+		if ( ! $project ) {
+			WP_CLI::error( 'Project not found' );
+		}
 
 		if ( ! $project ) {
 			return new WP_Error( '404', 'Could not find project for this repository' );

commit b6f6cebbed32f67d5891726c00f7d6bc44f42ff2

Fix undefined method error in ProjectLocator
Introduced in 17f3ba46255cecb1109338b15f406f129d55faca.

inc/ProjectLocator.php
@@ -18,6 +18,8 @@ use PO;
  */
 class ProjectLocator {
 	/**
+	 * Possible GlotPress project ID or path or GitHub repository path.
+	 *
 	 * @var string|int Project information.
 	 */
 	protected $project;
@@ -44,7 +46,7 @@ class ProjectLocator {
 		}
 
 		if ( ! $project ) {
-			$project = GitHubUpdater::find_project( $this->project );
+			$project = $this->find_by_github_repository_url();
 		}
 
 		return $project;
@@ -56,7 +58,7 @@ class ProjectLocator {
 	 * @since 2.0.0
 	 * @return false|GP_Project Project on success, false otherwise.
 	 */
-	protected function find_project() {
+	protected function find_by_github_repository_url() {
 		global $wpdb;
 
 		$table = GP::$project->table;