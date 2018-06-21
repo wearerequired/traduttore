d638762f583b6f18b3a43e63d129dc5524edc072
Pascal Birchler <hello@pascalbirchler.ch>
2018-06-21 07:02:43 +0200

    Begin with phpunit setup

---
+++ tests/phpunit/tests/ProjectLocator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class GetPushResources
+ *
+ * @package H2push
+ */
+
+namespace Required\Traduttore\Tests;
+
+use \WP_UnitTestCase;
+use \Required\Traduttore\ProjectLocator as PL;
+
+/**
+ *  Test cases for \Required\Traduttore\ProjectLocator.
+ */
+class ProjectLocator extends WP_UnitTestCase {
+
+}
