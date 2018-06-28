2cc9879d8cff58fdfe3abf5653505897bf938846
Pascal Birchler <hello@pascalbirchler.ch>
2018-06-28 12:56:57 +0200

    Make sure GlotPress doesn't bail early

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -21,6 +21,10 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	exit( 1 );
 }
 
+if ( ! file_exists( $_gp_tests_dir . '/bootstrap.php' ) ) {
+	die( "GlotPress test suite could not be found. Have you run bin/install-wp-tests.sh ?\n" );
+}
+
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
@@ -29,6 +33,11 @@ tests_add_filter( 'muplugins_loaded', function() use ( $_gp_tests_dir ) {
 
 	require dirname( dirname( __DIR__ ) ) . '/traduttore.php';
 } );
+
+global $wp_tests_options;
+
+// So GlotPress doesn't bail early, see https://github.com/GlotPress/GlotPress-WP/blob/43bb5383e114835b09fc47c727d06e6d3ca8114e/glotpress.php#L142-L152.
+$wp_tests_options['permalink_structure'] = '/%postname%';
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
