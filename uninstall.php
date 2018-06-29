5f46b3e6e5e66d4bb83ec32f0e97a101207aa51a
Pascal Birchler <hello@pascalbirchler.ch>
2018-06-29 11:05:34 +0200

    Use WP_Filesystem on uninstall

--- uninstall.php
+++ uninstall.php
@@ -9,10 +9,23 @@ namespace Required\Traduttore;
 
 defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
 
+/* @var WP_Filesystem_Base $wp_filesystem */
+global $wp_filesystem;
+
+if ( ! $wp_filesystem ) {
+	require_once ABSPATH . '/wp-admin/includes/admin.php';
+
+	if ( ! \WP_Filesystem() ) {
+		return false;
+	}
+}
+
+$wp_filesystem->rmdir( $file_or_folder, true );
+
 array_map(
-	function ( $file_or_folder ) {
-			is_dir( $file_or_folder ) ? rmdir( $file_or_folder ) : unlink( $file_or_folder );
+	function ( $file_or_folder ) use ( $wp_filesystem ) {
+		$wp_filesystem->delete( $file_or_folder, true, is_dir( $file_or_folder ) ? 'd' : 'f' );
 	}, glob( get_temp_dir() . 'traduttore-*' )
 );
 
-rmdir( ZipProvider::get_cache_dir() );
+$wp_filesystem->rmdir( ZipProvider::get_cache_dir(), true );
