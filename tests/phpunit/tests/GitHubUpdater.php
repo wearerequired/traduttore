15d36154a7ad70f279feea69b57f213ff45f1bcf
Pascal Birchler <hello@pascalbirchler.ch>
2018-06-29 12:10:41 +0200

    Add test for GitHubUpdater::fetch_and_update()

--- tests/phpunit/tests/GitHubUpdater.php
+++ tests/phpunit/tests/GitHubUpdater.php
@@ -7,6 +7,7 @@
 
 namespace Required\Traduttore\Tests;
 
+use \GP;
 use \GP_UnitTestCase;
 use \WP_REST_Request;
 use \Required\Traduttore\GitHubUpdater as Updater;
@@ -20,6 +21,11 @@ class GitHubUpdater extends GP_UnitTestCase {
 	 */
 	protected $project;
 
+	/**
+	 * @var Updater
+	 */
+	protected $updater;
+
 	public function setUp() {
 		parent::setUp();
 
@@ -30,22 +36,30 @@ class GitHubUpdater extends GP_UnitTestCase {
 				'source_url_template' => 'https://github.com/wearerequired/traduttore/blob/master/%file%#L%line%',
 			]
 		);
+
+		$this->updater = new Updater( $this->project );
+		$this->updater->remove_local_repository();
 	}
 
 	public function test_get_repository_path() {
-		$updater = new Updater( $this->project );
-
-		$this->assertSame( get_temp_dir() . 'traduttore-github-sample-project', $updater->get_repository_path() );
+		$this->assertSame( get_temp_dir() . 'traduttore-github-sample-project', $this->updater->get_repository_path() );
 	}
 
 	public function test_remove_local_repository() {
-		$updater = new Updater( $this->project );
+		mkdir( $this->updater->get_repository_path() );
+		touch( $this->updater->get_repository_path() . '/foo.txt' );
 
-		mkdir( $updater->get_repository_path() );
-		touch( $updater->get_repository_path() . '/foo.txt' );
+		$this->assertTrue( file_exists( $this->updater->get_repository_path() . '/foo.txt' ) );
+		$this->assertTrue( $this->updater->remove_local_repository() );
+		$this->assertFalse( file_exists( $this->updater->get_repository_path() . '/foo.txt' ) );
+	}
 
-		$this->assertTrue( file_exists( $updater->get_repository_path() . '/foo.txt' ) );
-		$this->assertTrue( $updater->remove_local_repository() );
-		$this->assertFalse( file_exists( $updater->get_repository_path() . '/foo.txt' ) );
+	public function test_fetch_and_update() {
+		$result = $this->updater->fetch_and_update();
+
+		$originals = GP::$original->by_project_id( $this->project->id );
+
+		$this->assertTrue( $result );
+		$this->assertNotEmpty( $originals );
 	}
 }
