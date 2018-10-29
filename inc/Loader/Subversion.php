8a64c787aec8400f50c830f5c648b700d24c60ec
Pascal Birchler <hello@pascalbirchler.ch>
2018-10-29 14:32:02 +0200

    Add PoC subversion loader

---
+++ inc/Loader/Subversion.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Subversion loader class.
+ *
+ * @since 3.0.0
+ *
+ * @package Required\Traduttore
+ */
+
+namespace Required\Traduttore\Loader;
+
+use Required\Traduttore\Repository;
+
+/**
+ * Subversion loader class.
+ *
+ * @since 3.0.0
+ */
+class Subversion extends Base {
+	/**
+	 * Downloads a remote Subversion repository.
+	 *
+	 * If the repository has been checked out before, the latest changes will be pulled.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return string Path to the downloaded repository on success.
+	 */
+	public function download(): ?string {
+		if ( is_dir( $this->get_local_path() ) ) {
+			$this->update_existing_repository();
+		}
+
+		return $this->download_new_repository();
+	}
+
+	/**
+	 * Downloads a fresh copy of a remote repository.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return null|string Path to the downloaded repository on success.
+	 */
+	protected function download_new_repository(): ?string {
+		$target = $this->get_local_path();
+
+		exec(
+			escapeshellcmd(
+				sprintf(
+					'svn checkout %1$s %2$s -q',
+					escapeshellarg( $this->get_checkout_url() ),
+					escapeshellarg( $target )
+				)
+			),
+			$output,
+			$status
+		);
+
+		return 0 === $status ? $target : null;
+
+	}
+
+	/**
+	 * Updates an existing copy of a remote repository.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return null|string Path to the downloaded repository on success.
+	 */
+	protected function update_existing_repository(): ?String {
+		$target = $this->get_local_path();
+
+		$current_dir = getcwd();
+		chdir( $target );
+		exec( escapeshellcmd( 'svn revert --recursive .' ), $output, $status );
+		exec( escapeshellcmd( 'svn update .' ), $output, $status );
+		chdir( $current_dir );
+
+		return 0 === $status ? $target : null;
+	}
+
+	/**
+	 * Returns the URL to check out the current repository.
+	 *
+	 * Supports HTTPS and SSH URLs.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return string URL to check out the repository, e.g. svn+ssh://svn.example.com/wearerequired/traduttore
+	 *                or https://svn.example.com/wearerequired/traduttore.
+	 */
+	protected function get_checkout_url(): string {
+		/**
+		 * Filters whether HTTPS or SSH should be used to check out a Subversion repository.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @param bool       $use_https  Whether to use HTTPS instead of SSH for checking out Subversion repositories.
+		 *                               Defaults to true for public repositories.
+		 * @param Repository $repository The current repository.
+		 */
+		$use_https = apply_filters( 'traduttore.svn_checkout_use_https', $this->repository->is_public(), $this->repository );
+
+		$checkout_url = $this->repository->get_ssh_url();
+
+		if ( $use_https ) {
+			$checkout_url = $this->repository->get_https_url();
+		}
+
+		/**
+		 * Filters the URL used to check out a Subversion repository.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @param string     $checkout_url The URL to check out a Subversion repository.
+		 * @param Repository $repository   The current repository.
+		 */
+		return apply_filters( 'traduttore.svn_checkout_url', $checkout_url, $this->repository );
+	}
+}
---
+++ tests/phpunit/tests/Loader/Subversion.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Class Subversion
+ *
+ * @package Traduttore\Tests
+ */
+
+namespace Required\Traduttore\Tests;
+
+use \GP_UnitTestCase;
+use \Required\Traduttore\Loader\Subversion as SubversionLoader;
+use \Required\Traduttore\Project;
+use Required\Traduttore\Repository\Bitbucket;
+
+/**
+ * Test cases for \Required\Traduttore\Loader\Subversion.
+ *
+ * @todo Mock shell execution
+ */
+class Subversion extends GP_UnitTestCase {
+	/**
+	 * @var Project
+	 */
+	protected $project;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->project = new Project(
+			$this->factory->project->create(
+				[
+					'name'                => 'Sample Project',
+					'slug'                => 'sample-project',
+					'source_url_template' => 'https://bitbucket.org/wearerequired/traduttore/src/master/%file%#L%line%',
+				]
+			)
+		);
+	}
+
+	public function test_get_local_path(): void {
+		$loader = new SubversionLoader( new Bitbucket( $this->project ) );
+
+		$this->assertStringEndsWith( 'traduttore-bitbucket.org-wearerequired-traduttore', $loader->get_local_path() );
+	}
+
+	public function test_download_repository(): void {
+		$this->markTestSkipped( 'Need to mock shell command execution' );
+
+		$loader = new SubversionLoader( new Bitbucket( $this->project ) );
+
+		add_filter( 'traduttore.svn_checkout_use_https', '__return_true' );
+		$result = $loader->download();
+		remove_filter( 'traduttore.svn_checkout_use_https', '__return_true' );
+
+		$this->assertSame( $result, $loader->get_local_path() );
+	}
+
+	public function test_download_existing_repository(): void {
+		$this->markTestSkipped( 'Need to mock shell command execution' );
+
+		$loader = new SubversionLoader( new Bitbucket( $this->project ) );
+
+		add_filter( 'traduttore.svn_checkout_use_https', '__return_true' );
+		$result1 = $loader->download();
+		$result2 = $loader->download();
+		remove_filter( 'traduttore.svn_checkout_use_https', '__return_true' );
+
+		$this->assertSame( $result1, $loader->get_local_path() );
+		$this->assertSame( $result2, $loader->get_local_path() );
+	}
+}
