53cd01dd0e74fabfc849e336e0c20e717df83a23
Dominik Schilling <dominikschilling+git@gmail.com>
2018-11-10 14:39:23 +0100

    Add mapping functionality from w.org for building JSON files

--- inc/ZipProvider.php
+++ inc/ZipProvider.php
@@ -127,6 +127,31 @@ class ZipProvider {
 			return false;
 		}
 
+		// Build a mapping based on where the translation entries occur and separate the po entries.
+		$mapping    = $this->build_mapping( $entries );
+		$po_entries = array_key_exists( 'po', $mapping ) ? $mapping['po'] : [];
+		unset( $mapping['po'] );
+
+		// Create JSON file(s).
+		$json_file_base = "{$dest}/{$wp_locale}";
+		$jed_files      = $this->build_json_files( $project, $locale, $this->translation_set, $mapping, $json_file_base );
+
+		// Create PO file.
+		$po_file = "{$dest}/{$wp_locale}.po";
+		$result  = $this->build_po_file( $project, $locale, $this->translation_set, $po_entries, $po_file );
+
+		if ( ! $result ) {
+			return false;
+		}
+
+		// Create MO file.
+		$mo_file = "{$dest}/{$wp_locale}.mo";
+		$result  = $this->build_mo_file( $project, $locale, $this->translation_set, $po_entries, $mo_file );
+
+		if ( ! $result ) {
+			return false;
+		}
+
 		$files_for_zip = [];
 
 		/* @var GP_Format $format */
@@ -167,6 +192,114 @@ class ZipProvider {
 		 * @param GP_Translation_Set $translation_set Translation set the language pack is for.
 		 */
 		do_action( 'traduttore.zip_generated', $this->get_zip_path(), $this->get_zip_url(), $this->translation_set );
+
+		return true;
+	}
+
+	/**
+	 * Build a mapping of JS files to translation entries occurring in those files.
+	 * Translation entries occurring in other files are added to the 'po' key.
+	 *
+	 * @param Translation_Entry[] $entries The translation entries to map.
+	 * @return array The mapping of sources to translation entries.
+	 */
+	private function build_mapping( $entries ) {
+		$mapping = array();
+
+		foreach ( $entries as $entry ) {
+			/** @var Translation_Entry $entry */
+
+			// Find all unique sources this translation originates from.
+			$sources = array_map( function ( $reference ) {
+				$parts = explode( ':', $reference );
+				$file  = $parts[0];
+
+				if ( substr( $file, -7 ) === '.min.js' ) {
+					return substr( $file, 0, -7 ) . '.js';
+				}
+
+				if ( substr( $file, -3 ) === '.js' ) {
+					return $file;
+				}
+
+				return 'po';
+			}, $entry->references );
+
+			// Always add all entries to the PO file.
+			$sources[] = 'po';
+
+			$sources = array_unique( $sources );
+
+			foreach ( $sources as $source ) {
+				$mapping[ $source ][] = $entry;
+			}
+		}
+
+		return $mapping;
+	}
+
+	/**
+	 * Builds a mapping of JS file names to translation entries.
+	 *
+	 * @param GP_Project          $gp_project The GlotPress project.
+	 * @param GP_Locale           $gp_locale  The GlotPress locale.
+	 * @param GP_Translation_Set  $set        The translation set.
+	 * @param array               $mapping    A mapping of files to translation entries.
+	 * @param string              $base_dest  Destination file name.
+	 * @return array An array of translation files built, may be empty if no translations in JS files exist.
+	 */
+	private function build_json_files( $gp_project, $gp_locale, $set, $mapping, $base_dest ) : array {
+		// Export translations for each JS file to a separate translation file.
+		$files  = [];
+		$format = gp_array_get( GP::$formats, 'jed1x' );
+		foreach ( $mapping as $file => $entries ) {
+			$json_content = $format->print_exported_file( $gp_project, $gp_locale, $set, $entries );
+
+			$hash = md5( $file );
+			$dest = "{$base_dest}-{$hash}.json";
+
+			file_put_contents( $dest, $json_content );
+
+			$files[] = $dest;
+		}
+
+		return $files;
+	}
+
+	/**
+	 * Builds a PO file for translations.
+	 *
+	 * @param GP_Project          $gp_project The GlotPress project.
+	 * @param GP_Locale           $gp_locale  The GlotPress locale.
+	 * @param GP_Translation_Set  $set        The translation set.
+	 * @param Translation_Entry[] $entries    The translation entries.
+	 * @param string              $dest       Destination file name.
+	 * @return boolean True on success, false on failure.
+	 */
+	private function build_po_file( $gp_project, $gp_locale, $set, $entries, $dest ) : bool {
+		$format     = gp_array_get( GP::$formats, 'po' );
+		$po_content = $format->print_exported_file( $gp_project, $gp_locale, $set, $entries );
+
+		file_put_contents( $dest, $po_content );
+
+		return true;
+	}
+
+	/**
+	 * Builds a MO file for translations.
+	 *
+	 * @param GP_Project          $gp_project The GlotPress project.
+	 * @param GP_Locale           $gp_locale  The GlotPress locale.
+	 * @param GP_Translation_Set  $set        The translation set.
+	 * @param Translation_Entry[] $entries    The translation entries.
+	 * @param string              $dest       Destination file name.
+	 * @return boolean True on success, false on failure.
+	 */
+	private function build_mo_file( $gp_project, $gp_locale, $set, $entries, $dest ) : bool {
+		$format     = gp_array_get( GP::$formats, 'mo' );
+		$mo_content = $format->print_exported_file( $gp_project, $gp_locale, $set, $entries );
+
+		file_put_contents( $dest, $mo_content );
 
 		return true;
 	}
