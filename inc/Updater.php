ca13bf4495ab31380b9a88610019a6388b007a85
Pascal Birchler <hello@pascalbirchler.ch>
2018-11-12 11:14:08 +0100

    Only set text domain qafter successful import

--- inc/Updater.php
+++ inc/Updater.php
@@ -121,13 +121,13 @@ class Updater {
 		$translations = new PO();
 		$result       = $translations->import_from_file( $pot_file );
 
-		$this->project->set_text_domain( sanitize_text_field( $translations->headers['X-Domain'] ) );
-
 		unlink( $pot_file );
 
 		if ( ! $result ) {
 			return false;
 		}
+
+		$this->project->set_text_domain( sanitize_text_field( $translations->headers['X-Domain'] ) );
 
 		$stats = GP::$original->import_for_project( $this->project->get_project(), $translations );
 
