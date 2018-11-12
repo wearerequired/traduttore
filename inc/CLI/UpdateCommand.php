f7bffe7d7883a4af14e8e9f93e43508c49e243e7
Pascal Birchler <hello@pascalbirchler.ch>
2018-11-12 11:15:58 +0100

    Re-add information about new flag to docblock

--- inc/CLI/UpdateCommand.php
+++ inc/CLI/UpdateCommand.php
@@ -30,6 +30,9 @@ class UpdateCommand extends WP_CLI_Command {
 	 * <project|url>
 	 * : Project path / ID or source code repository URL, e.g. https://github.com/wearerequired/required-valencia
 	 *
+	 * [--cached]
+	 * : Used cached repository information and do not try to download code from remote.
+	 *
 	 * [--delete]
 	 * : Whether to first delete the existing local repository or not.
 	 *
