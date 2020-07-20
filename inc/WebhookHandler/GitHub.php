5fe5c5948b4b9c5fb337cf74e43b118cb1619259
Dominik Schilling <dominikschilling+git@gmail.com>
2020-07-20 13:12:44 +0200

    Support `application/x-www-form-urlencoded` as content type for GitHub webhook

--- inc/WebhookHandler/GitHub.php
+++ inc/WebhookHandler/GitHub.php
@@ -63,11 +63,21 @@ class GitHub extends Base {
 	 * @return \WP_Error|\WP_REST_Response REST response on success, error object on failure.
 	 */
 	public function callback() {
-		$params     = $this->request->get_params();
 		$event_name = $this->request->get_header( 'x-github-event' );
 
 		if ( 'ping' === $event_name ) {
 			return new WP_REST_Response( [ 'result' => 'OK' ] );
+		}
+
+		$params       = $this->request->get_params();
+		$content_type = $this->request->get_content_type();
+		// See https://developer.github.com/webhooks/creating/#content-type.
+		if ( ! empty( $content_type ) && 'application/x-www-form-urlencoded' === $content_type['value'] ) {
+			$params = json_decode( $params['payload'], true );
+		}
+
+		if ( ! isset( $params['repository']['default_branch'] ) ) {
+			return new \WP_Error( '400', 'Request incomplete', [ 'status' => 400 ] );
 		}
 
 		// We only care about the default branch but don't want to send an error still.
@@ -79,7 +89,7 @@ class GitHub extends Base {
 		$project = $locator->get_project();
 
 		if ( ! $project ) {
-			return new \WP_Error( '404', 'Could not find project for this repository' );
+			return new \WP_Error( '404', 'Could not find project for this repository', [ 'status' => 404 ] );
 		}
 
 		$project->set_repository_name( $params['repository']['full_name'] );
