88d8bbebead5a925c4c02abb59813f30e61c18e0
Pascal Birchler <pascalb@google.com>
2024-05-30 14:09:49 +0200

    Lint fixes
    [dependabot skip]

--- tests/phpunit/utils/TestCase.php
+++ tests/phpunit/utils/TestCase.php
@@ -7,7 +7,6 @@ namespace Required\Traduttore\Tests\Utils;
 
 use GP_UnitTest_Factory;
 use GP_UnitTestCase;
-use WP_Error;
 use WP_REST_Response;
 
 /**
@@ -29,8 +28,12 @@ class TestCase extends GP_UnitTestCase {
 
 	/**
 	 * @see WP_Test_REST_TestCase
+	 *
+	 * @param int|string                  $code Error code.
+	 * @param \WP_REST_Response|\WP_Error $response Response.
+	 * @param int|null                    $status Status code.
 	 */
-	protected function assertErrorResponse( mixed $code, WP_REST_Response|WP_Error $response, mixed $status = null ): void {
+	protected function assertErrorResponse( mixed $code, WP_REST_Response|\WP_Error $response, mixed $status = null ): void {
 		if ( $response instanceof WP_REST_Response ) {
 			$response = $response->as_error();
 		}
